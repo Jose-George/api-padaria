19a40f9e72e1582639852fa86a52686c08aa2a4b
Edivan Silva <164213518+EdivanS96@users.noreply.github.com>
2025-12-10 15:38:07 -0300

    Inclusão do app products com modelo Supplier, API CRUD e rotas base configuradas

--- api_padaria/settings.py
+++ api_padaria/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products', #Add app products By Edivan
     'rest_framework',
 ]
 
