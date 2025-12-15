4de4232b947b0f7e13fdd27f7ba0a1672e1d6858
lillyamdl <lillyamaria980@gmail.com>
2025-12-15 18:47:49 -0300

    feat: adiciona modelo Product

--- products/models.py
+++ products/models.py
@@ -14,3 +14,22 @@ class Supplier(models.Model):
     class Meta:
         verbose_name = "Fornecedor"
         verbose_name_plural = "Fornecedores"
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    supplier = models.ForeignKey(
+        Supplier,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    qtd_atual = models.IntegerField(default=0)
+    qtd_minima = models.IntegerField(default=0)
+    validade = models.DateField()
+    lote = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
