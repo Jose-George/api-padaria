06921e1bbf96d3390ded4209c988aed50bc70bbc
lillyamdl <lillyamaria980@gmail.com>
2026-01-19 18:08:56 -0300

    feat: Adiciona campo supplier_id ao ProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Product
 
+from rest_framework import serializers
+from .models import Product
+from suppliers.models import Supplier
+
 class ProductSerializer(serializers.ModelSerializer):
+    supplier_id = serializers.PrimaryKeyRelatedField(
+        queryset=Supplier.objects.all(),
+        source='supplier',
+        write_only=True
+    )
+
     class Meta:
         model = Product
         fields = '__all__'
+        depth = 1
