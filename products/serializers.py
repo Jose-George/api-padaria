2a9f6996a35cfcc2ffe12682cbee75ef5d599bf5
lillyamdl <lillyamaria980@gmail.com>
2026-01-19 18:12:51 -0300

    refactor: Remove código comentado no ProductSerializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 from .models import Product
-
-from rest_framework import serializers
-from .models import Product
 from suppliers.models import Supplier
 
 class ProductSerializer(serializers.ModelSerializer):
